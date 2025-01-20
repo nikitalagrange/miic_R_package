18908ea08a1288b29baae461794ad58a613081e2
Nikita Lagrange <109518744+nlagrang@users.noreply.github.com>
2025-01-20 17:15:14 +0100

    Update get_information.h

--- src/get_information.h
+++ src/get_information.h
@@ -11,6 +11,16 @@ structure::InfoBlock getCondMutualInfo(int X, int Y,
     const std::vector<int>& ui_list, const structure::Grid2d<int>& data_numeric,
     const structure::Grid2d<int>& data_numeric_idx, structure::Environment&,
     std::shared_ptr<structure::CutPointsInfo> = nullptr);
+structure::InfoBlock getEdgeScore(int X, int Y,
+const std::vector<int>& ui_list, const structure::Grid2d<int>& data_numeric,
+const structure::Grid2d<int>& data_numeric_idx, structure::Environment&,
+std::shared_ptr<structure::CutPointsInfo> = nullptr);
+double getComplexityOrient(int X, int Y,
+const std::vector<int>& ui_list, const structure::Grid2d<int>& data_numeric,
+const structure::Grid2d<int>& data_numeric_idx, structure::Environment&);
+    structure::InfoBlock getCondEntropy(int X,
+    const std::vector<int>& ui_list, const structure::Grid2d<int>& data_numeric,
+    const structure::Grid2d<int>& data_numeric_idx, structure::Environment&);
 structure::Info3PointBlock getInfo3Point(structure::Environment&, int X, int Y,
     int Z, const std::vector<int>& ui_list);
 void searchForBestContributingNode(
